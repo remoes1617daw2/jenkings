9ac079d223c473922f7c5c6927ef38db82b3a801
remoes1617daw2 <remoes1617daw2@gmail.com>
2018-02-02 12:28:25 +0100

    Update Jenkins.java

--- src/Jenkins.java
+++ src/Jenkins.java
@@ -23,9 +23,11 @@ public class Jenkins {
             bw.write("  </head>");bw.newLine();
             bw.write("  <body>");bw.newLine();
             bw.write("    Projecte per provar l'IC Jenkins");bw.newLine();
+            bw.write("    2a Modificació del projecte jenkins);bw.newLine()
             bw.write("  </body>");bw.newLine();
             bw.write("</html>");bw.newLine();
             bw.close();
+            
         }
     }
 }
